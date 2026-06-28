e36105de1cb33d3a4969270283cfab92688dd77f
shxyj111 <shxyj111@gmail.com>
2026-06-28 21:34:42 +0800

    ``` feat(web): 添加登录页面并配置应用设置
    - 在settings.py中导入os模块并配置模板目录
    - 注册web应用到INSTALLED_APPS中
    - 创建登录页面模板文件，包含完整的HTML结构和CSS样式
    - 更新login视图函数，渲染登录页面模板
    ```

--- my_django_site/web/views.py
+++ my_django_site/web/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.shortcuts import HttpResponse
 
 def login(request):
-    return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
+    return render(request, 'web/login.html')
+    # return redirect('https://www.baidu.com')
